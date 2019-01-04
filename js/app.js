43dea026670b75df16d67a1732b20d9c3d1f353d
bolajiayodeji <sirbeejay1@gmail.com>
2019-01-04 23:42:04 +0100

    added api calls

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 let rent_head = `<div class="container">
-            <div class="row">
-            <div class="row intro_results_row">
-            <div class="intro_results_dots magic_fade_in"></div>`;
+<div class="row">`;
 let rent = ``;
 let rent_foot = `</div>
-            </div>
-            </div>
-            </div>`;
+</div>`;
+
 $.ajax({
     type: 'GET',
     url: 'https://murmuring-forest-71544.herokuapp.com/api/electronics',
@@ -19,20 +16,23 @@ $.ajax({
             let numberInStock = data[i].numberInStock;
 
 
-            //UI variables
-            let title = '';
-            let effect_time = '';
 
-
-            let toAppend = `<div class='rent_result col-lg-4 intro_results_col align-items-center magic_fade_in'>
-                            <div class='intro_results_item d-flex flex-column align-items-center justify-content-start text-center'>
-                              <div class='card' style='width: 20rem;'>
-                                  <img id='upnepa_bulb' class='card-img-top' src="${bulb}" alt="Card image cap">
-                                  <h5>${title}</h5>
-                              <h5>${effect_time}</h5>
-                               </div>
-                            </div>
-                            </div>`;
+            let toAppend = `<!-- Rent Card -->
+            <div class="col-lg-3 col-md-6 col-12">
+                <div class="card mb-5 mb-lg-0">
+                    <div class="card-body">
+                        <h5 class="card-title text-muted text-uppercase text-center">${name}</h5>
+                        <img src="img/logo.png" class="card-details text-center img-fluid">
+                        <hr>
+                        <ul class="fa-ul">
+                            <li><span class="fa-li"><i class="fas fa-caret-right"></i></span>${description}</li>
+                            <li><span class="fa-li"><i class="fas fa-caret-right"></i></span>${dailyRentalFee}</li>
+                            <li><span class="fa-li"><i class="fas fa-caret-right"></i></span>${numberInStock}</li>
+                        </ul>
+                        <a href="#" class="btn btn-block btn-primary text-uppercase">Rent Now</a>
+                    </div>
+                </div>
+            </div>`;
 
             rent += toAppend;
             $('.content').html(rent_head + rent + rent_foot);
