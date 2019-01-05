e7d4e00336c2431cc0b510dbaffe1300137d0dd3
bolajiayodeji <sirbeejay1@gmail.com>
2019-01-05 19:37:59 +0100

    added workbox

--- js/sw.js
+++ js/sw.js
@@ -1,45 +1,64 @@
-self.addEventListener('fetch', function (event) {
-    event.respondWith(caches.open('cache').then(function (cache) {
-        return cache.match(event.request).then(function (response) {
-            console.log('cache request: ' + event.request.url);
-            var fetchPromise = fetch(event.request).then(function (networkResponse) {
-                // if we got a response from the cache, update the cache                   
-                console.log('fetch completed: ' + event.request.url, networkResponse);
-                if (networkResponse) {
-                    console.debug('updated cached page: ' + event.request.url, networkResponse);
-                    cache.put(event.request, networkResponse.clone());
-                }
-                return networkResponse;
-            }, function (event) {
-                // rejected promise - just ignore it, we're offline!   
-                console.log('Error in fetch()', event);
-                event.waitUntil(
-                    caches.open('cache').then(function (cache) {
-                        return cache.addAll([
-                            '/index.html',
-                            '/index.html?homescreen=1',
-                            '/?homescreen=1',
-                            '/css/app.css',
-                            '/js/app.js',
-                            '/img/logo.png',
-                            '/manifest.js',
-                            '/https://murmuring-forest-71544.herokuapp.com/api',
-                            '/https://murmuring-forest-71544.herokuapp.com/api/electronics',
-                            '/https://murmuring-forest-71544.herokuapp.com/api/electronics/categories',
-                            '/https://use.fontawesome.com/releases/v5.6.3/css/all.css',
-                            '/https://fonts.googleapis.com/css?family=Poppins:200,300,400,500',
-                        ]);
-                    })
-                );
-            });
-            // respond from the cache, or the network
-            return response || fetchPromise;
-        });
-    }));
-});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js');
 
-self.addEventListener('install', function (event) {
-    // The promise that skipWaiting() returns can be safely ignored.
-    self.skipWaiting();
-    console.log('Latest version installed!');
-};
+if (workbox) {
+    console.log('Yay! Workbox is loaded ! Rentify 🔥');
+    workbox.precaching.precacheAndRoute([]);
+
+    /*  cache images */
+    workbox.routing.registerRoute(
+        /(.*)others(.*)\.(?:png|gif|jpg)/,
+        workbox.strategies.networkFirst({
+            cacheName: 'images',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 50, // max number of images to cache
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                })
+            ]
+        })
+    );
+    /* Makes JS and CSS ⚡ fast by returning the assets from the cache, 
+  while making sure they are updated in the background for the next use.
+  */
+    workbox.routing.registerRoute(
+        // cache js, css, scc files 
+        /.*\.(?:css|js|scss|)/,
+        // use cache but update in the background ASAP
+        workbox.strategies.staleWhileRevalidate({
+            // use a custom cache name
+            cacheName: 'assets',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    // max number of items to be cached
+                    maxEntries: 20,
+                }),
+            ]
+        })
+    );
+
+    // cache google fonts
+    workbox.routing.registerRoute(
+        new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+        workbox.strategies.networkFirst({
+            cacheName: 'google-fonts',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        })
+    );
+
+    // add offline analytics 
+    workbox.googleAnalytics.initialize();
+
+    /* Install a new service worker and have it update 
+    and control a web page as soon as possible
+    */
+
+    workbox.skipWaiting();
+    workbox.clientsClaim();
+
+} else {
+    console.log('Oops! Workbox didn\'t load 👺');
+}
