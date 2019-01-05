b874010f33ec83e146b794f0a820aa9247945584
bolajiayodeji <sirbeejay1@gmail.com>
2019-01-06 00:17:08 +0100

    fixed eslint errors

--- register/userRegister.js
+++ register/userRegister.js
@@ -1,11 +1,11 @@
-$("#signup").click(function () {
-    $("#first").fadeOut("fast", function () {
-        $("#second").fadeIn("fast");
+$('#signup').click(function () {
+    $('#first').fadeOut('fast', function () {
+        $('#second').fadeIn('fast');
     });
 });
 
-$("#signin").click(function () {
-    $("#second").fadeOut("fast", function () {
-        $("#first").fadeIn("fast");
+$('#signin').click(function () {
+    $('#second').fadeOut('fast', function () {
+        $('#first').fadeIn('fast');
     });
 });
