058e9935ac5dcf3f2839f5e1db054f92be1dee51
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-11 00:51:12 +0100

    removed nav duplicate

--- rent-product.php
+++ rent-product.php
@@ -66,49 +66,7 @@
 
 <body>
 
-  <nav class="navbar sticky-top centered navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="/">
-      <img src="img/logo.png" width="70" height="auto" alt="">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="about-us">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="rent-product">Rent Now</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="add-product">Submit Product</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            FAQ
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search">
-        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </nav><!--Header-->
+    <!--Header-->
     <?php include 'includes/header.php'?>
 
     <!-- STYLESHEETS-->
@@ -130,7 +88,7 @@
 
 
   <!-- Footer -->
-  <?php include '../includes/footer.php'?>
+  <?php include 'includes/footer.php'?>
    <!-- Footer -->
 
   <!--==================== JavaScript ==============================-->
