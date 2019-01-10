59d8698b55a54660fa8b16591042ce0fbc87173f
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-01-11 00:30:54 +0100

    setup php includes

---
+++ includes/header.php
@@ -0,0 +1,51 @@
+<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Twitter Card-->
+    <meta name="twitter:title" content="Rentify">
+    <meta name="twitter:site" content="" />
+    <meta name="twitter:creator" content="" />
+    <meta name="twitter:description" content="Rentify is a virtual renting platform that allows you to rent your electronics and book rentals.">
+    <meta name="twitter:image" content="img/logo.png">
+    <meta name="twitter:card" content="">
+    <meta name="description" content="Rentify is a virtual renting platform that allows you to rent your electronics and book rentals.">
+    <meta name="keywords" content="Rentify, Rentals, Electronics, Devices, Gadgets, Favcode54" />
+    <!-- Facebook card -->
+    <meta property="fb:admins" content="" />
+    <meta property="fb:page_id" content="" />
+    <!--Open Graph-->
+    <meta property="og:image" content="img/logo.png">
+    <meta property="og:title" content="Rentify" />
+    <meta property="og:description" content="" />
+    <meta name="author" content="Rentify">
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="Rentify" />
+    <meta property="og:type" content="Rentify is a virtual renting platform that allows you to rent your electronics and book rentals." />
+
+    <!-- Add to home screen for Safari on iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-mobile-web-app-title" content="Rentify" />
+    <link rel="apple-touch-icon" href="img/icons/icon-152x152.png" />
+    <!-- Windows -->
+    <meta name="msapplication-TileImage" content="/img/icons/icon-144x144.png" />
+    <meta name="msapplication-TileColor" content="#007bff" />
+
+    <link rel="manifest" href="/manifest.json" />
+    <meta name="theme-color" content="#007bff" />
+
+    <!-- Google Knowledge Graph card -->
+    <script type='application/ld+json'>
+        {
+            "@context": "https://www.schema.org",
+            "@type": "",
+            "name": "Rentify ()",
+            "url": "",
+            "logo": "img/logo.png",
+            "description": "Rentify is a virtual renting platform that allows you to rent your electronics and book rentals."
+        }
+    </script>
+
+    <!--FAVICON-->
+    <link rel="icon" href="img/logo.png" type="image/png">
+    <link rel="shortcut icon" href="" type="img/x-icon">
